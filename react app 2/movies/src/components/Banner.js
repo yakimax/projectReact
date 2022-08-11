3e070f98de3eb487d4884902a394b4e8acc16d1b
yakimax <sv1099912@gmail.com>
2022-08-11 22:29:06 +0530

    add

--- react app 2/movies/src/components/Banner.js
+++ react app 2/movies/src/components/Banner.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export class Banner extends Component {
   render() {
     return (
-      <div>Banner</div>
+        <div className="card banner-card">
+        <img src="https://m.media-amazon.com/images/I/71mUK6cPosL._SL1280_.jpg" class="card-img-top banner-img" alt="..."></img>
+          <h5 className="card-title banner-title">Mars</h5>
+          <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
     )
   }
 }
--- react app 2/movies/src/components/NavBar.js
+++ react app 2/movies/src/components/NavBar.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export class NavBar extends Component {
   render() {
     return (
-        <div>
+        <div style={{display : 'flex' , padding : '0.5rem'}}>
             <h1>Movies App</h1>
-            <h1>Favourites</h1>
+            <h1 style={{marginLeft : '2.0rem' , margintop : '1.8rem'}}>Favourites</h1>
         </div>
     )
   }
