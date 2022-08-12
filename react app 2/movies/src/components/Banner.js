c22e3766ff5142344f1f8aae3fdd515bcb9cf666
yakimax <sv1099912@gmail.com>
2022-08-12 18:13:01 +0530

    commit

--- react app 2/movies/src/components/Banner.js
+++ react app 2/movies/src/components/Banner.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export class Banner extends Component {
   render() {
+    let backdrop = `/6EdKBYkB1ssgGjc249ud1L55o8d.jpg` ;
     return (
         <div className="card banner-card">
-        <img src="https://m.media-amazon.com/images/I/71mUK6cPosL._SL1280_.jpg" class="card-img-top banner-img" alt="..."></img>
-          <h5 className="card-title banner-title">Mars</h5>
-          <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
+        <img src={`https://image.tmdb.org/t/p/original${backdrop}`} class="card-img-top banner-img" alt="..."></img>
+          <h5 className="card-title banner-title">Jurassic Hunt</h5>
+          <p className="card-text banner-text">'Female adventurer Parker joins a crew of male trophy hunters in a remote wilderness park. Their goal: slaughter genetically recreated dinosaurs for sport using rifles, arrows, and grenades. After their guide is killed by raptors, the team tries to escape the park – but the hunters quickly become the hunted. Even worse, the park’s manager suspects Parker of being a spy and sends a hit squad after her. This battle’s about to become primitive!'</p>
+        </div>
     )
   }
 }
--- react app 2/movies/src/components/Movielist.js
+++ react app 2/movies/src/components/Movielist.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
 import {movies} from '../movie'
 export class Movielist extends Component {
+  
+  constructor(){
+    super() ;
+    this.state = {
+      hover: ''
+    }
+  }
   render() {
-    let movieArr = movies.results
+
+    let movieArr = movies.results ;
+
     return (
-        movieArr.map((movieObj) => (
-            <h1>{movieObj.title}</h1>
-        ))
+      <>
+      <div>
+        <h3 classname = "text-center"><strong>Trending</strong></h3>
+      </div>
+      <div className = 'movie-list'>
+        {
+            movieArr.map((movieObj) => (
+            <div className="card movie-card" onMouseEnter={ () => {this.setState({hover : movieObj.id})}} onMouseLeave ={()=>{this.setState({hover : '' })}} >
+                
+                <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} style={{height : '40vh' ,width : '20vw'}} className ="card-img-top movie-img" alt="..."></img>
+                <h5 className="card-title movie-title">{movieObj.title}</h5>
+                
+                <div className = 'button-wrapper' style={{display : 'flex' , justifyContent : 'center'}}>
+                { 
+                    this.state.hover == movieObj.id &&
+                    <a href="#" className="btn btn-primary movie-button text-center">Add to Favourites</a>
+                }
+                </div>
+            
+            </div>
+            ))
+        }
+      </div>
+      <div className = 'text-center pagination' style={{ display : 'flex' , justifyContent : 'center' }}>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
+      </div>
+      </>
     )
   }
 }
