3e65cc13fa8d30b6d45975d8556198c358e72341
yakimax <sv1099912@gmail.com>
2022-08-13 00:02:32 +0530

    add

--- react app 2/movies/src/components/Banner.js
+++ react app 2/movies/src/components/Banner.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+import { movies } from '../movie';
 
 export class Banner extends Component {
   render() {
-    let backdrop = `/6EdKBYkB1ssgGjc249ud1L55o8d.jpg` ;
+    let movieElement = movies.results[Math.floor(Math.random()*movies.results.length) + 1] ;
+    let backdrop = movieElement.backdrop_path ;
+    let title = movieElement.title ;
+    let description = movieElement.overview ;
     return (
         <div className="card banner-card">
         <img src={`https://image.tmdb.org/t/p/original${backdrop}`} class="card-img-top banner-img" alt="..."></img>
-          <h5 className="card-title banner-title">Jurassic Hunt</h5>
-          <p className="card-text banner-text">'Female adventurer Parker joins a crew of male trophy hunters in a remote wilderness park. Their goal: slaughter genetically recreated dinosaurs for sport using rifles, arrows, and grenades. After their guide is killed by raptors, the team tries to escape the park – but the hunters quickly become the hunted. Even worse, the park’s manager suspects Parker of being a spy and sends a hit squad after her. This battle’s about to become primitive!'</p>
+          <h5 className="card-title banner-title">{title}</h5>
+          <p className="card-text banner-text">{description}</p>
         </div>
     )
   }
--- react app 2/movies/src/components/Movielist.js
+++ react app 2/movies/src/components/Movielist.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react'
-import {movies} from '../movie'
+// import {movies} from '../movie'
+import axios from 'axios'
+
 export class Movielist extends Component {
   
   constructor(){
     super() ;
+    console.log('constructor first')
     this.state = {
-      hover: ''
+      hover: '',
+      paginationArr : [1] ,
+      movies : []
     }
   }
-  render() {
 
-    let movieArr = movies.results ;
+  async componentDidMount(){
+
+    
+      const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=f3d5cc67afc578246bf41c05b08e3164&language=en-US&page=1');
+      let responseData = res.data ;
+      console.log('CDN third');
+      console.log(responseData) ;
+      this.setState({
+        movies : [...responseData.results]
+      })
+  }
+
+  render() {
+    console.log('render second');
 
     return (
+
+
+
       //fragment started
 
       <>
-      //first element in movie-card---------------------------------
+      //first element in movie-card----------------------------------------------
       <div>
-        <h3 classname = "text-center"><strong>Trending</strong></h3>
+        <h3 className = "text-center"><strong>Trending</strong></h3>
       </div>
       
 
       //second element in movie-card----------------------------------------------------------------------------------------------------------------------------
       <div className = 'movie-list'>
         {
-            movieArr.map((movieObj) => (
+            this.state.movies.map((movieObj) => (
             <div className="card movie-card" onMouseEnter={ () => {this.setState({hover : movieObj.id})}} onMouseLeave ={()=>{this.setState({hover : '' })}} >
                 
                 <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} style={{height : '40vh' ,width : '20vw'}} className ="card-img-top movie-img" alt="..."></img>
@@ -44,14 +64,16 @@ export class Movielist extends Component {
       </div>
       
 
-      //third element in movie-card --------------------------------------------------------------------
+      //third element in movie-card --------------------------------------------------------------------------------
       <div className = 'text-center pagination' style={{ display : 'flex' , justifyContent : 'center' }}>
           <nav aria-label="Page navigation example">
             <ul class="pagination">
               <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              {
+                  this.state.paginationArr.map((pagenum)=>{
+                    <li class="page-item"><a class="page-link" href="#">{pagenum}</a></li>
+                  })
+              }
               <li class="page-item"><a class="page-link" href="#">Next</a></li>
             </ul>
           </nav>
