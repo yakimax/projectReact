3a871a04b7a7dd35bf81c8ad2365d57179ebc041
yakimax <sv1099912@gmail.com>
2022-09-01 19:58:37 +0530

    add

--- react  functional/functional/src/App.js
+++ react  functional/functional/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test'
+// import Test from './components/Test'
 import useEffect1 from './components/useEffect1' ;
 
 function App() {
---
+++ react  functional/functional/src/components/useEffect1.js
@@ -0,0 +1,10 @@
+import React ,{useState} from 'react'
+
+function useEffect1() {
+
+  return (
+    <div></div>
+  )
+}
+
+export default useEffect1
