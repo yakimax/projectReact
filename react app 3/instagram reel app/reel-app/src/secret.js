2d7e7ff467a2100ef744fc7a3ddf4197987e1525
yakimax <sv1099912@gmail.com>
2022-09-16 20:28:43 +0530

    add

---
+++ react app 3/instagram reel app/reel-app/src/secret.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANUUTzasxOhBrsvBDDSjQiOMdStU2R17E",
+    authDomain: "project-lifter-reels.firebaseapp.com",
+    projectId: "project-lifter-reels",
+    storageBucket: "project-lifter-reels.appspot.com",
+    messagingSenderId: "1066991427374",
+    appId: "1:1066991427374:web:2c80e5b4d175311b6ca4bf",
+    measurementId: "G-MFQDKXB9M9"
+  };
+
+  module.exports = firebaseConfig ;
