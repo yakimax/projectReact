98b92c673643bad017fb6f0d5705b47b9558814e
yakimax <sv1099912@gmail.com>
2022-09-20 23:03:30 +0530

    add

--- react app 3/instagram reel app/reel-app/src/components/Login.jsx
+++ react app 3/instagram reel app/reel-app/src/components/Login.jsx
@@ -1,24 +1,50 @@
 import { React , useState } from 'react' ;
+import {auth} from '../firebase';
+import {signInWithEmailAndPassword} from 'firebase/auth'
 
 function Login(){
   const [email,setEmail] = useState('') ;
   const [pass,setPass] = useState('') ;
-let trackmail = (e)=>{
-  setEmail(e.target.value)
+  const [err,setError] = useState('') ;
+
+  const [loader,setLoader] = useState(false) ;
+  const [user,setUser] = useState(null) ;
+
+  let trackmail = (e)=>{
+  setEmail(e.target.value) ;
 }
 let trackpass = (e)=>{
-  setPass(e.target.value);
+  setPass(e.target.value) ;
 }
-let displayDetails =()=>{
-  alert(email +"    "+pass)
+let displayDetails = async ()=>{
+  try{
+    setLoader(true);
+    let usersignin = await signInWithEmailAndPassword(auth,email,pass) ;
+    setUser(usersignin.user);
+  }catch(err){
+    setError(err.message) ;
+    setTimeout(()=>{
+      setError("") ;
+  },2000)
+  }
+  setLoader(false) ;  
 }
+
   return (
     <>
-      <input type = "text" value = {email} placeholder = 'email' onChange={trackmail}></input>
-      <br></br>
-      <input type = "text" value = {pass} placeholder = 'password' onChange={trackpass}></input>
-      <br></br>
-      <button type = "click" onClick={displayDetails}>Log in</button>
+    {
+      err !== "" ? <h1>Error is{err}</h1> 
+      : loader === true ? <h1>...Loading</h1> 
+      : user != null ? <h1>User is @{user.uid}</h1> 
+      : <>
+        <input type = "text" value = {email} placeholder = 'email' onChange={trackmail}></input>
+        <br></br>
+        <input type = "text" value = {pass} placeholder = 'password' onChange={trackpass}></input>
+        <br></br>
+        <button type = "click" onClick={displayDetails}>Log in</button>
+      </>
+    }
+    
     </>
   )
 }
--- react app 3/instagram reel app/reel-app/src/firebase.js
+++ react app 3/instagram reel app/reel-app/src/firebase.js
@@ -1,7 +1,9 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 
-import secret from './secrets';
+import secret from './secret';
 
+let app = initializeApp(secret);
 
+export let auth = getAuth(app);
 
