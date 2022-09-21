63149114f8cacb5f2fd44a6b73a794ea0037d09b
yakimax <sv1099912@gmail.com>
2022-09-22 00:33:28 +0530

    addd

--- react app 3/instagram reel app/reel-app/src/components/Profile.jsx
+++ react app 3/instagram reel app/reel-app/src/components/Profile.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import '../App.css'
 
 function Profile() {
-  return 
-  (
+  return (
     <>
       <div className='header'></div>
         <div className="main">
-          <div className="profileImage"></div>
+          <div className="img_container">
+            <img src = "https://picsum.photos/200" className='pimg' alt=""></img>
+          </div>
           <div className="details">
-            <div className="content"></div>
-            <div className="content"></div>
-            <div className="content"></div>
+            <div className="content">Name :</div>
+            <div className="content">No of posts :</div>
+            <div className="content">Mail me :</div>
           </div>
       </div>
     </>
