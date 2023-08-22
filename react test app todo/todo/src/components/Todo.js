29104ccdffd4dd01d6b59928e4c70cf171b4abc2
yakimax <sv1099912@gmail.com>
2023-08-22 14:22:34 +0530

    add

--- react test app todo/todo/src/components/Todo.js
+++ react test app todo/todo/src/components/Todo.js
@@ -4,7 +4,7 @@ export default class Todo extends Component {
     constructor(){
         super();
         this.state = {
-            tasks : [{value : "aalu lena hai",id: 0}] 
+            tasks : [{value : "aalu lena hai",id: 0}] ,
             currTask: "" 
         }
     }
@@ -17,7 +17,7 @@ export default class Todo extends Component {
 
     handleSubmit = () =>{
         this.setState({
-            tasks : [...this.state.tasks,{value : this.state.currTask , id : this.state.tasks.length+1}]
+            tasks : [...this.state.tasks,{value : this.state.currTask , id : this.state.tasks.length+1}],
             currTask : "" 
         })
     }
@@ -25,19 +25,21 @@ export default class Todo extends Component {
     render() {
     return (
     <>
-      <div>Todo List </div> <br>
-      <input type="text" value={this.state.currTask} onChange={handleChange}></input>
-      <button onClick={this.handleSubmit}>Add Task</button><br>
-      <ul>
-        {
-            this.state.tasks.map((Obj)=>(
-                <li key={Obj.id}>
-                    <p>{Obj.value}</p>
-                    <button onChange={this.handleDelete}>Delete task</button>
-                </li><br>
-            ))
-        }
-      </ul>
+        <div>Todo List </div>
+        <input type="text" value={this.state.currTask} onChange={this.handleChange}></input>
+        <button onClick={this.handleSubmit}>Add Task</button>
+        <ul>
+            {
+                this.state.tasks.map((Obj)=>(
+                    <li key={Obj.id}>
+                        <>
+                        <p>{Obj.value}</p>
+                        <button onChange={this.handleDelete}>Delete task</button>
+                        </>
+                    </li>
+                ))
+            }
+        </ul>
     </>
     )
   }
