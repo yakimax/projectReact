c4db98878b76b810abab5d5c6e4fecc90b0dc0c7
yakimax <sv1099912@gmail.com>
2023-08-25 13:31:00 +0530

    add

--- lifterReelsApp/liftreels/src/contextAPIs/AuthContext.js
+++ lifterReelsApp/liftreels/src/contextAPIs/AuthContext.js
@@ -1,5 +1,23 @@
-import {useState,useEffect} from 'react' ;
+import {useState,useEffect,useContext} from 'react' ;
 export const AuthContext = React.createContext() ;
+import auth from '../firebase/auth' ;
 
+
+function AuthProvider(){
+    const [user,setUser] = useContext('') ;
+    const [loading,setLoading] = useContext(true) ;
+
+    function login(){
+        
+    }
+
+    function signout(){
+        return auth.signout() ;
+    }
+
+    function signup(){
+        // return auth.
+    }
+}
 
 
